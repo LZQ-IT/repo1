208cdb9fdd345969375cfe8091f085b87638a5ab
liziqiang <liziqiang@qq.com>
2020-04-06 15:14:29 +0800

    测试git导入项目

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 public class Main {
 
     @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+    public void testHelloWrold() { System.out.println("HelloWorld!");
     }
 }
