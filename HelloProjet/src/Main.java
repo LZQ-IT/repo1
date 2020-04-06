9650f566d43dbf698316fabca2dcbff399d0e951
liziqiang <liziqiang@qq.com>
2020-04-06 15:33:22 +0800

    修改Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 public class Main {
 
     @Test
-    public void testHelloWrold() { System.out.println("HelloWorld!");
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
     }
 }
